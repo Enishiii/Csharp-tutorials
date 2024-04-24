382f25fde942dbf459d1b83114cecb3ec9cb9abf
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-04-24 23:07:19 +0900

    add other mathematical operations

--- NumbersInCSharp/obj/Debug/net8.0/NumbersInCSharp.AssemblyInfo.cs
+++ NumbersInCSharp/obj/Debug/net8.0/NumbersInCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumbersInCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731a3acc3ce2923e2e0e0097f0b0fc430952a843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a36054da140b7b29d87bf6d6d109edc5d9e80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumbersInCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumbersInCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
