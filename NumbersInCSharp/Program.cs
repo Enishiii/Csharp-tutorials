f9d522e1262faded0224b479156bee01493a9838
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-04-26 22:48:09 +0900

    OrderPrecedenceメソッドにコードを移動

--- NumbersInCSharp/Program.cs
+++ NumbersInCSharp/Program.cs
@@ -1,22 +1,5 @@
 ﻿// WorkWithIntegers();
-
-int a = 5;
-int b = 4;
-int c = 2;
-int d = a + b * c;
-Console.WriteLine(d);
-
-d = (a + b) * c;
-Console.WriteLine(d);
-
-d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-Console.WriteLine(d);
-
-int e = 7;
-int f = 4;
-int g = 3;
-int h = (e + f) / g;
-Console.WriteLine(h);
+OrderPrecedence();
 
 void WorkWithIntegers()
 {
@@ -33,4 +16,25 @@ void WorkWithIntegers()
 
     c = a / b;
     Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
 }
