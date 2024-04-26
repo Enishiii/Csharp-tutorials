eb9031464264c693ce7a780738408b321f0e5d17
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-04-27 00:55:53 +0900

    double型のrounding errorを検証

--- NumbersInCSharp/Program.cs
+++ NumbersInCSharp/Program.cs
@@ -18,6 +18,9 @@ double max = double.MaxValue;
 double min = double.MinValue;
 Console.WriteLine($"The range of double is {min} to {max}");
 
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+
 void WorkWithIntegers()
 {
     int a = 18;
