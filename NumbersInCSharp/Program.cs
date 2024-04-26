7283e62af87a6da5afa6cb356e513dd020819cca
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-04-26 23:04:00 +0900

    余りの計算を追加

--- NumbersInCSharp/Program.cs
+++ NumbersInCSharp/Program.cs
@@ -1,6 +1,14 @@
 ﻿// WorkWithIntegers();
 OrderPrecedence();
 
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
 void WorkWithIntegers()
 {
     int a = 18;
