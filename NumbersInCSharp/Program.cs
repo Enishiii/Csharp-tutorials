e26c50db0483717a699c53b31f33358fc0450623
Enishi Yoshioka <enishiyoshioka@gmail.com>
2024-04-27 01:08:03 +0900

    decimal型の計算コードをメソッドに移動

--- NumbersInCSharp/Program.cs
+++ NumbersInCSharp/Program.cs
@@ -2,18 +2,7 @@
 // OrderPrecedence();
 // TestLimits();
 // WorkWithDoubles();
-
-decimal min = decimal.MinValue;
-decimal max = decimal.MaxValue;
-Console.WriteLine($"The range of the decimal type is {min} to {max}");
-
-double a = 1.0;
-double b = 3.0;
-Console.WriteLine(a / b);
-
-decimal c = 1.0M;
-decimal d = 3.0M;
-Console.WriteLine(c / d);
+WorkWithDecimals();
 
 void WorkWithIntegers()
 {
@@ -91,4 +80,19 @@ void WorkWithDoubles()
 
     double third = 1.0 / 3.0;
     Console.WriteLine(third);
+}
+
+void WorkWithDecimals()
+{
+    decimal min = decimal.MinValue;
+    decimal max = decimal.MaxValue;
+    Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a / b);
+
+    decimal c = 1.0M;
+    decimal d = 3.0M;
+    Console.WriteLine(c / d);
 }
