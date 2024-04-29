c0fc56a9c29d898d677a409c5186d7253a5b86a7
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-04-29 20:55:57 +0900

    文字列リストを作成し、foreachで出力するコードを追加

--- ListCollections/Program.cs
+++ ListCollections/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+List<string> names = ["Enishi", "Ana", "Felipe"];
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
