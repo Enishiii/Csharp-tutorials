eafa65d0963668f2d35682ae54d023ba1a1f073c
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-04-29 20:20:55 +0900

    while文を追加

--- BranchesAndLoops/Program.cs
+++ BranchesAndLoops/Program.cs
@@ -1,4 +1,11 @@
-﻿ExploreIf();
+﻿// ExploreIf();
+
+int counter = 0;
+while (counter < 10)
+{
+    Console.WriteLine($"Hello World! The counter is {counter}");
+    counter++;
+}
 
 void ExploreIf()
 {
