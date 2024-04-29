3072ce91c62c1c2ba634fa828dec121bcb832c49
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-04-29 20:59:06 +0900

    リスト要素の追加・削除を追加

--- ListCollections/Program.cs
+++ ListCollections/Program.cs
@@ -5,3 +5,12 @@ foreach (var name in names)
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");
 }
+
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
