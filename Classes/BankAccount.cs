3f6587ccdd8d2006abea5de7a37d09799918a1c6
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-04-30 20:44:04 +0900

    BankAccountクラスのオブジェクトを作成＆typoを修正

--- Classes/BankAccount.cs
+++ Classes/BankAccount.cs
@@ -6,11 +6,17 @@ public class BankAccount
     public string Owner { get; set; }
     public decimal Balance { get; }
 
-    public void MakeDeposit(decimal amount, Datetime date, string note)
+    public BankAccount(string name, decimal initialBalance)
+    {
+        Owner = name;
+        Balance = initialBalance;
+    }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
     {
     }
 
-    public void MakeWithdrawal(decimal amount, Datetime date, string note)
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
     {
     }
 }
