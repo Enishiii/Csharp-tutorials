d8a02562bf1c2f6685935a86675d3fd5bfae9f0a
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-04-30 20:18:44 +0900

    20番目までのフィボナッチ数列を出力

--- ListCollections/Program.cs
+++ ListCollections/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 WorkWithStrings();
 
 List<int> fibonacciNumbers = [1, 1];
 
-var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
-
-fibonacciNumbers.Add(previous + previous2);
+while(fibonacciNumbers.Count < 20)
+{
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+    
+    fibonacciNumbers.Add(previous + previous2);
+}
 
 foreach (var item in fibonacciNumbers)
 {
