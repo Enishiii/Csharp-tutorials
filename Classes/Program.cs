5ab77eb1d8afb9978b8d1cc11fcca9b9d0517c6e
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-05-01 21:09:16 +0900

    全てのトランザクションのログを残す実装を追加

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,24 +10,26 @@ account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
 Console.WriteLine(account.Balance);
 
 // Test for a negative balance.
-try
-{
-    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-}
-catch (InvalidOperationException e)
-{
-    Console.WriteLine("Exception caught trying to overdraw");
-    Console.WriteLine(e.ToString());
-}
+// try
+// {
+//     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+// }
+// catch (InvalidOperationException e)
+// {
+//     Console.WriteLine("Exception caught trying to overdraw");
+//     Console.WriteLine(e.ToString());
+// }
 
-BankAccount invalidAccount;
-try
-{
-    invalidAccount = new BankAccount("invalid", -55);
-}
-catch (ArgumentOutOfRangeException e)
-{
-    Console.WriteLine("Exception caught creating account with negative balance");
-    Console.WriteLine(e.ToString());
-    return;
-}
+// BankAccount invalidAccount;
+// try
+// {
+//     invalidAccount = new BankAccount("invalid", -55);
+// }
+// catch (ArgumentOutOfRangeException e)
+// {
+//     Console.WriteLine("Exception caught creating account with negative balance");
+//     Console.WriteLine(e.ToString());
+//     return;
+// }
+
+Console.WriteLine(account.GetAccountHistory());
