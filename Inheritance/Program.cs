5e730a8fa3f56fa3bce2ccd9efb774c5946fb55f
EnishiYoshioka <yoshioka@youworks.co.jp>
2024-05-02 22:00:26 +0900

    privateが親の中で入れ子になった子クラスからしかアクセスできないことを検証

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class A 
+{
+    private int _value = 10;
+
+    public class B : A 
+    {
+        public int GetValue()
+        {
+            return _value;
+        }
+    }
+}
+
+public class C : A 
+{
+    public int GetValue()
+    {
+        return _value;
+    }
+}
+
+public class AccessExample
+{
+    public static void Main(string[] args)
+    {
+        var b = new A.B();
+        Console.WriteLine(b.GetValue());
+    }
+}
